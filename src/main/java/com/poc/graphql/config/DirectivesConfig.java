7e00a9cb37fe676031f1c311bd4ce5e3c65caa6e
Seba-Ach <33301421+Seba-Ach@users.noreply.github.com>
2022-09-21 16:10:31 +0200

    Squashed commit of the following:
    commit bd8bda86e7a3b5c1b5b5e8d3b7b0fa9fca0c3b86
    Author: Seba-Ach <33301421+Seba-Ach@users.noreply.github.com>
    Date:   Wed Sep 21 16:10:10 2022 +0200
    
        directives with validation for size and not blank

---
+++ src/main/java/com/poc/graphql/config/DirectivesConfig.java
@@ -0,0 +1,21 @@
+package com.poc.graphql.config;
+
+import graphql.validation.rules.OnValidationErrorStrategy;
+import graphql.validation.rules.ValidationRules;
+import graphql.validation.schemawiring.ValidationSchemaWiring;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DirectivesConfig {
+
+    @Bean
+    public ValidationSchemaWiring validationSchemaWiring(){
+        ValidationRules validationRules = ValidationRules.newValidationRules()
+                .onValidationErrorStrategy(OnValidationErrorStrategy.RETURN_NULL)
+                .build();
+
+        return new ValidationSchemaWiring(validationRules);
+    }
+
+}
